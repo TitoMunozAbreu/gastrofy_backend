58afe7c95ceff0b62032b9448d0600234b01ee63
titomunoz <munoztitojose@gmail.com>
2024-02-11 00:01:21 +0100

    new Gastrofy project

---
+++ src/main/java/com/app/gastrofy_backend/GastrofyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.gastrofy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrofyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastrofyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/gastrofy_backend/GastrofyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gastrofy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrofyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
