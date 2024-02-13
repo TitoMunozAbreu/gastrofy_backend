fcd6d91f4be2325a2860d117107e5821d50441d5
titomunoz <munoztitojose@gmail.com>
2024-02-13 21:27:51 +0100

    create a new usuario

---
+++ src/main/java/com/app/gastrofy_backend/controller/impl/UsuarioController.java
@@ -0,0 +1,23 @@
+package com.app.gastrofy_backend.controller.impl;
+
+import com.app.gastrofy_backend.controller.UsuarioApi;
+import com.app.gastrofy_backend.model.request.UsuarioRequest;
+import com.app.gastrofy_backend.model.response.HttpGlobalResponse;
+import com.app.gastrofy_backend.model.response.UsuarioResponse;
+import com.app.gastrofy_backend.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController implements UsuarioApi {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @Override
+    public ResponseEntity<HttpGlobalResponse<UsuarioResponse>> registrarUsuario(UsuarioRequest usuarioDto) {
+        return usuarioService.registrarUsuario(usuarioDto);
+    }
+}
