ba868f4df14c6b389231eb10a71218f139ee4b95
titomunoz <munoztitojose@gmail.com>
2024-02-13 21:25:47 +0100

    usuario Dtos to handle requests and responses

---
+++ src/main/java/com/app/gastrofy_backend/model/request/UsuarioRequest.java
@@ -0,0 +1,9 @@
+package com.app.gastrofy_backend.model.request;
+
+public record UsuarioRequest(String email,
+                             String nombre,
+                             String apellido,
+                             String contrasena,
+                             String movil,
+                             String rol,
+                             String nombreEmpresa) {}
---
+++ src/main/java/com/app/gastrofy_backend/model/response/UsuarioResponse.java
@@ -0,0 +1,17 @@
+package com.app.gastrofy_backend.model.response;
+
+import lombok.*;
+
+import static lombok.AccessLevel.PUBLIC;
+
+@Data
+@NoArgsConstructor(access = PUBLIC)
+@AllArgsConstructor
+@Builder
+public class UsuarioResponse {
+    private String email;
+    private String nombre;
+    private String apellido;
+    private String movil;
+    private String rol;
+}
