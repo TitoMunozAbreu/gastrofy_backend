555161aeaaa812a35ea4a617b0e633a19bfbc414
titomunoz <munoztitojose@gmail.com>
2024-02-16 07:49:16 +0100

    added repositories

---
+++ src/main/java/com/app/gastrofy_backend/repositories/SistemaCostoRepository.java
@@ -0,0 +1,9 @@
+package com.app.gastrofy_backend.repositories;
+
+import com.app.gastrofy_backend.model.SistemaCosto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SistemaCostoRepository extends JpaRepository<SistemaCosto, Integer> {
+    //comprobar si existe sistema registrado con email
+    Boolean existsByNombreEmpresa(String nombreEmpresa);
+}
--- src/main/java/com/app/gastrofy_backend/repositories/UsuarioRepository.java
+++ src/main/java/com/app/gastrofy_backend/repositories/UsuarioRepository.java
@@ -4,4 +4,6 @@ import com.app.gastrofy_backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    //comprobar si existe usuario registrado con email
+    Boolean existsByEmail(String email);
 }
