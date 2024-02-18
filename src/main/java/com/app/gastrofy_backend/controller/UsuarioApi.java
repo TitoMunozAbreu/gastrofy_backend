9ddaaf05c8fd904e56b3db5d357d3e4358d1b490
titomunoz <munoztitojose@gmail.com>
2024-02-18 11:12:55 +0100

    added listarUsuarios method

--- src/main/java/com/app/gastrofy_backend/controller/UsuarioApi.java
+++ src/main/java/com/app/gastrofy_backend/controller/UsuarioApi.java
@@ -1,14 +1,18 @@
 package com.app.gastrofy_backend.controller;
 
 import com.app.gastrofy_backend.exceptions.DuplicateResourceException;
+import com.app.gastrofy_backend.exceptions.ResourceNotFoundException;
 import com.app.gastrofy_backend.model.response.HttpGlobalResponse;
 import com.app.gastrofy_backend.model.request.UsuarioRequest;
 import com.app.gastrofy_backend.model.response.UsuarioResponse;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 import static com.app.gastrofy_backend.controller.constante.EndpointUri.Api.Usuarios.USUARIOS;
 
@@ -18,4 +22,9 @@ public interface UsuarioApi {
     @PostMapping
     ResponseEntity<HttpGlobalResponse<UsuarioResponse>> registrarUsuario(@RequestBody
                                                               @Valid UsuarioRequest usuarioDto) throws DuplicateResourceException;
+
+    //listar usuarios
+    @GetMapping
+    ResponseEntity<HttpGlobalResponse<Page<UsuarioResponse>>> listarUsuarios(@RequestParam Optional<String> usuarioNombre,
+                                                                             @PageableDefault Pageable pageable) throws ResourceNotFoundException;
 }
