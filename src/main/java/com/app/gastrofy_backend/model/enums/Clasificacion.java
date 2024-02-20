1b675f980bdaa536398f239e97b4ffc85bd7a7af
titomunoz <munoztitojose@gmail.com>
2024-02-20 19:19:46 +0100

    enums updated

--- src/main/java/com/app/gastrofy_backend/model/enums/Clasificacion.java
+++ src/main/java/com/app/gastrofy_backend/model/enums/Clasificacion.java
@@ -2,9 +2,7 @@ package com.app.gastrofy_backend.model.enums;
 
 public enum Clasificacion {
     PRODUCCION ("PRODUCCION"),
-    NO_PRODUCCION ("NO PRODUCCION"),
-    NO_APLICA ("NO APLICA");
-
+    ADMINISTRATIVO ("ADMINISTRATIVO");
     private String descripcion;
 
     Clasificacion(String descripcion) {
--- src/main/java/com/app/gastrofy_backend/model/enums/Periodo.java
+++ src/main/java/com/app/gastrofy_backend/model/enums/Periodo.java
@@ -2,5 +2,6 @@ package com.app.gastrofy_backend.model.enums;
 
 public enum Periodo {
     MENSUAL,
-    QUINCENAL
+    QUINCENAL,
+    SEMANAL
 }
